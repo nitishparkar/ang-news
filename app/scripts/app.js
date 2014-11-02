65c6fbfbb94355d2a161c9084fb9cfefe008fa8d
Nitish Parkar <developer.nitish@gmail.com>
2014-11-02 19:28:41 +0530

    added single post view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,10 @@ var app = angular
         templateUrl: 'views/posts.html',
         controller: 'PostsCtrl'
       })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,14 +1,14 @@
 'use strict';
 
-app.controller('PostsCtrl', function($scope, Post) {
+app.controller('PostsCtrl', function($scope, $location, Post) {
   var INITIAL_POST = {title: '', url: 'http://'};
 
   $scope.posts = Post.all;
   $scope.post = angular.copy(INITIAL_POST);
 
   $scope.submitPost = function() {
-    Post.create($scope.post).then(function() {
-      $scope.post = angular.copy(INITIAL_POST);
+    Post.create($scope.post).then(function(ref) {
+      $location.path('/posts/' + ref.name());
     });
   };
 
