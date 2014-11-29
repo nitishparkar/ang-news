8e8efba3526547cea2c2e871b1995ca328f6b341
Nitish Parkar <developer.nitish@gmail.com>
2014-11-29 20:34:46 +0530

    done with the angular part of thinkster.io tutorial, starting rails

--- angular-rails/app.js
+++ angular-rails/app.js
@@ -1,18 +1,34 @@
-angular.module('whackerNews', [])
-.controller('MainCtrl', function($scope) {
+angular.module('whackerNews', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    }).state('post', {
+      url: '/posts/{id}',
+      templateUrl: '/post.html',
+      controller: 'PostCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+})
+.controller('MainCtrl', function($scope, postsFactory) {
   $scope.test = "Hello World";
 
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = postsFactory.posts;
 
   $scope.addPost = function() {
     if($scope.title && $scope.title !== '') {
-      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
       $scope.title = '';
       $scope.link = '';
     }
@@ -21,4 +37,30 @@ angular.module('whackerNews', [])
   $scope.upvote = function(post) {
     post.upvotes += 1;
   }
+})
+.controller('PostCtrl', function($scope, postsFactory, $stateParams) {
+  $scope.post = postsFactory.posts[$stateParams.id];
+
+  $scope.addComment = function() {
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  }
+})
+.factory('postsFactory', function() {
+  var ret = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ]
+  };
+
+  return ret;
 });
